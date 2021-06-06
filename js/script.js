56bc4c4c69eca19d4ddbad916967a0de4e6328eb
DamianoD88 <dispe88@live.it>
2021-06-06 23:45:35 +0200

    modifica..10

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+
+document.getElementById('genera').addEventListener("click", function(){
+document.getElementById('ticket').innerHTML = 'biglietto';
+})
+// carrozza e cp calcolare random
+
+var carrozza = 3;
+
+var codice = 95003;
+
+
+
+// output
+
+document.getElementById('offerta').innerHTML = fascia;
+document.getElementById('carrozza').innerHTML = carrozza; 
+document.getElementById('codice').innerHTML = codice;
+document.getElementById('sconto').innerHTML = prezzo;
